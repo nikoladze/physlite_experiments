24ebf3022d98ff57ebd1112492609df3b9a006f4
Nikolai Hartmann <nikoladze@posteo.de>
2020-10-11 17:30:04 +0200

    extend to read structs to support vector<vector<ElementLink<...

--- deserialization_hacks.py
+++ deserialization_hacks.py
@@ -29,9 +29,9 @@ def _read_big_endian_int(data):
 
 
 @numba.njit
-def _read_vector_vector_int(basket_data, border, num_entries):
+def _read_vector_vector(basket_data, border, num_entries, data_size=4, data_header_size=0):
     d = basket_data
-    buf_size = len(basket_data) // 4
+    buf_size = len(basket_data) // data_size # conservative estimate
     offsets_outer = np.empty(num_entries + 1, dtype=np.int64)
     offsets_inner = np.empty(buf_size, dtype=np.int64)
     offsets_outer[0] = 0
@@ -49,11 +49,15 @@ def _read_vector_vector_int(basket_data, border, num_entries):
         offsets_outer[i_outer] = offsets_outer[i_outer - 1] + n_outer
         while inner_start < stop:
             n = _read_big_endian_int(d[inner_start: inner_start + 4])
-            inner_stop = inner_start + n * 4 + 4
+            inner_stop = inner_start + n * (data_size + data_header_size) + 4
             offsets_inner[total_entries] = offsets_inner[total_entries - 1] + n
-            for i in range(inner_start + 4, inner_stop):
-                actual_data[total_bytes] = d[i]
-                total_bytes += 1
+            i = inner_start + 4
+            while i < inner_stop:
+                i += data_header_size
+                for ii in range(data_size):
+                    actual_data[total_bytes] = d[i + ii]
+                    total_bytes += 1
+                i += data_size
             total_entries += 1
             inner_start = inner_stop
         i_outer += 1
@@ -61,12 +65,16 @@ def _read_vector_vector_int(basket_data, border, num_entries):
     return offsets_outer, offsets_inner[:total_entries], actual_data[:total_bytes]
 
 
-def branch_to_array_vector_vector_int(branch):
+def branch_to_array_vector_vector(branch, dtype=np.dtype(">i4"), data_size=4, data_header_size=0):
     oo, oi, ad = [], [], []
     for i in range(branch.num_baskets):
         basket = branch.basket(i)
-        oo_i, oi_i, ad_i = _read_vector_vector_int(
-            basket.raw_data.tobytes(), basket.border, basket.num_entries
+        oo_i, oi_i, ad_i = _read_vector_vector(
+            basket.raw_data.tobytes(),
+            basket.border,
+            basket.num_entries,
+            data_size=data_size,
+            data_header_size=data_header_size
         )
         ad.append(ad_i)
         if len(oo) == 0:
@@ -77,16 +85,24 @@ def branch_to_array_vector_vector_int(branch):
             oo.append(oo_i[1:] + oo[-1][-1])
             oi.append(oi_i[1:] + oi[-1][-1])
     oo, oi, ad = [np.concatenate(i) for i in [oo, oi, ad]]
-    ad = np.frombuffer(ad.tobytes(), dtype=np.dtype(">i4"))
-    ad = np.array(ad, dtype="<i4")
+    ad = np.frombuffer(ad.tobytes(), dtype=dtype)
     return ak.Array(
         ak.layout.ListOffsetArray64(
             ak.layout.Index64(oo),
             ak.layout.ListOffsetArray64(
                 ak.layout.Index64(oi),
-                ak.layout.NumpyArray(ad)
+                ak.Array(ad.newbyteorder().byteswap()).layout
             )
         )
+    )
+
+def branch_to_array_vector_vector_int(branch):
+    return branch_to_array_vector_vector(branch, dtype=np.dtype(">i4"), data_size=4, data_header_size=0)
+
+
+def branch_to_array_vector_vector_elementlink(branch):
+    return branch_to_array_vector_vector(
+        branch, dtype=np.dtype([("m_persKey", ">i4"), ("m_persIndex", ">i4")]), data_size=8, data_header_size=20
     )
 
 
@@ -94,3 +110,9 @@ def test_vector_vector_int():
     with uproot4.open(example_file()) as f:
         branch = f["CollectionTree"]["AnalysisJetsAuxDyn.NumTrkPt500"]
         assert ak.all(branch.array() == branch_to_array_vector_vector_int(branch))
+
+
+def test_vector_vector_elementlink():
+    with uproot4.open(example_file()) as f:
+        branch = f["CollectionTree"]["AnalysisElectronsAuxDyn.trackParticleLinks"]
+        assert ak.all(branch.array() == branch_to_array_vector_vector_elementlink(branch))
