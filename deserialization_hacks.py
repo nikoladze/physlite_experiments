8d796c053263e2c0289160a27e394b8e78570e97
Nikolai Hartmann <nikoladze@posteo.de>
2020-10-16 15:39:23 +0200

    add support for vector<string>

--- deserialization_hacks.py
+++ deserialization_hacks.py
@@ -1,5 +1,5 @@
 import uproot4
-from uproot4 import AsObjects, AsVector
+from uproot4 import AsObjects, AsVector, AsString
 import os
 import numba
 import numpy as np
@@ -31,7 +31,19 @@ def _read_big_endian_int(data):
 
 
 @numba.njit
-def _read_vector_vector(basket_data, border, num_entries, data_size=4, data_header_size=0):
+def _read_vector_vector(basket_data, border, num_entries, data_size=4, data_header_size=0, offset_size=4):
+    """
+    Deserialize raw data bytes that represent a list of vector<vector<some_data_type>>
+
+    Parameters:
+    -----------
+    basket_data: array of bytes (eg. basket.raw_data)
+    border: last index containing any data (e.g. from basket.border, the rest will typically be the event byte offsets)
+    num_entries: number of events in this basket (e.g. from basket.num_entries)
+    data_size: number of bytes for each element
+    data_header_size: number of header bytes to skip over (e.g 20 for ElementLink)
+    offset_size: number of bytes that encode the offsets
+    """
     d = basket_data
     # estimate - might need to grow the inner offsets (can have many empty entries)
     buf_size = len(basket_data) // data_size
@@ -51,15 +63,15 @@ def _read_vector_vector(basket_data, border, num_entries, data_size=4, data_head
         n_outer = _read_big_endian_int(d[start + 6: start + 10])
         offsets_outer[i_outer] = offsets_outer[i_outer - 1] + n_outer
         while inner_start < stop:
-            n = _read_big_endian_int(d[inner_start: inner_start + 4])
-            inner_stop = inner_start + n * (data_size + data_header_size) + 4
+            n = _read_big_endian_int(d[inner_start: inner_start + offset_size])
+            inner_stop = inner_start + n * (data_size + data_header_size) + offset_size
             if total_entries >= len(offsets_inner):
                 # increase the size if not sufficient
                 offsets_inner = np.concatenate(
                     (offsets_inner, np.empty(buf_size, dtype=np.int64))
                 )
             offsets_inner[total_entries] = offsets_inner[total_entries - 1] + n
-            i = inner_start + 4
+            i = inner_start + offset_size
             while i < inner_stop:
                 i += data_header_size
                 for ii in range(data_size):
@@ -73,7 +85,7 @@ def _read_vector_vector(basket_data, border, num_entries, data_size=4, data_head
     return offsets_outer, offsets_inner[:total_entries], actual_data[:total_bytes]
 
 
-def _branch_to_array_vector_vector(branch, dtype=np.dtype(">i4"), data_size=4, data_header_size=0):
+def _branch_to_array_vector_vector(branch, dtype=np.dtype(">i4"), data_size=4, data_header_size=0, offset_size=4):
     oo, oi, ad = [], [], []
     for i in range(branch.num_baskets):
         basket = branch.basket(i)
@@ -82,7 +94,8 @@ def _branch_to_array_vector_vector(branch, dtype=np.dtype(">i4"), data_size=4, d
             basket.border,
             basket.num_entries,
             data_size=data_size,
-            data_header_size=data_header_size
+            data_header_size=data_header_size,
+            offset_size=offset_size
         )
         ad.append(ad_i)
         if len(oo) == 0:
@@ -113,6 +126,13 @@ def _branch_to_array_vector_vector_elementlink(branch):
     )
 
 
+def _branch_to_array_vector_string(branch):
+    array = _branch_to_array_vector_vector(branch, dtype=np.uint8, data_size=1, offset_size=1)
+    array.layout.content.setparameter("__array__", "string")
+    array.layout.content.content.setparameter("__array__", "char")
+    return array
+
+
 def interpretation_is_vector_vector(interpretation):
     "... there is probably a better way"
     if not isinstance(interpretation, AsObjects):
@@ -132,6 +152,8 @@ def interpretation_is_vector_vector(interpretation):
 
 def branch_to_array(branch, force_custom=False):
     "Try to deserialize with the custom functions and fall back to uproot"
+    if branch.interpretation == AsObjects(AsVector(True, AsString(False))):
+        return _branch_to_array_vector_string(branch)
     if interpretation_is_vector_vector(branch.interpretation):
         values = branch.interpretation._model.values.values
         if isinstance(values, np.dtype):
@@ -170,4 +192,10 @@ def test_vector_vector_float():
 def test_vector_vector_elementlink():
     with uproot4.open(example_file()) as f:
         branch = f["CollectionTree"]["AnalysisElectronsAuxDyn.trackParticleLinks"]
+        assert ak.all(branch.array() == branch_to_array(branch, force_custom=True))
+
+
+def test_vector_string():
+    with uproot4.open(example_file()) as f:
+        branch = f["CollectionTree"]["EventInfoAux.streamTagNames"]
         assert ak.all(branch.array() == branch_to_array(branch, force_custom=True))
